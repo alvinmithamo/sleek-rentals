5cafe1eca44ef547fa4450482613abe3d5a83a1c
alvinmithamo <ngangaalvin100@gmail.com>
2024-03-15 11:48:57 +0300

    frontend

---
+++ src/components/UI/AboutSection.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "../../styles/about-section.css";
+import aboutImg from "../../assets/all-images/cars-img/bmw-offer.png";
+
+const AboutSection = ({ aboutClass }) => {
+  return (
+    <section
+      className="about__section"
+      style={
+        aboutClass === "aboutPage"
+          ? { marginTop: "0px" }
+          : { marginTop: "280px" }
+      }
+    >
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="about__section-content">
+              <h4 className="section__subtitle">About Us</h4>
+              <h2 className="section__title">Welcome to car rent service</h2>
+              <p className="section__description">
+              Sleek Car Rentals is your one-stop shop for all your car rental needs. We are a company built on the desire to provide a smooth and convenient car rental experience, whether you're a local resident or just visiting.
+              </p>
+
+              <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Extensive Fleet
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Competitive Rates
+                </p>
+              </div>
+
+              <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Simple Booking 
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Exceptional Service
+                </p>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div className="about__img">
+              <img src={aboutImg} alt="" className="w-100" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default AboutSection;
